3d226dd2140281c5d994da310f9873a1057b768d
gdrtf <gdrtf@protonmail.com>
2020-05-20 21:19:02 +0900

    [Design] Tree bullet color

--- Scribs/src/app/components/sidebar/explorer/explorer.component.ts
+++ Scribs/src/app/components/sidebar/explorer/explorer.component.ts
@@ -9,30 +9,7 @@ import { WorkspaceContext } from 'src/app/pages/workspace/workspace.page';
 })
 export class ExplorerComponent implements OnInit {
   @Input() context: WorkspaceContext;
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
-    }
-  ];
+  nodes;
   options = {
     allowDrag: true,
     allowDrop: true
@@ -41,7 +18,7 @@ export class ExplorerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.nodes = [this.context.workspace.project];
+    this.nodes = this.context.workspace.project.children;
   }
 
 }
